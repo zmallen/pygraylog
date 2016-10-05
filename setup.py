8b9019f0a3082de8488dc33ff899c296dda80a76
Zack Allen <zma4580@gmail.com>
2016-10-05 11:59:39 -0400

    Botched a tag, rebumping

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from distutils.core import setup
 
 setup(name='pygraylog',
-      version='0.3',
+      version='0.3.1',
       description='Graylog API wrapper for python',
       author='Zack Allen',
       author_email='zma4580@gmail.com',
       url='https://www.github.com/zmallen/pygraylog',
-      download_url='https://github.com/zmallen/pygraylog/tarball/0.3',
+      download_url='https://github.com/zmallen/pygraylog/tarball/0.3.1',
       packages=['pygraylog'],
       keywords=['graylog', 'graylog-api', 'api graylog'],
       install_requires=[
